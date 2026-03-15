abaa9c97575687b65966b1965cf101633e975f1e
Sean Templeton <sean@templeton.io>
2026-03-15 11:02:18 -0500

    Hid window titlebar

--- src/client/java/com/hackwars/client/Application.kt
+++ src/client/java/com/hackwars/client/Application.kt
@@ -1,28 +1,39 @@
 package com.hackwars.client
 
+import java.awt.BorderLayout
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
 import javax.swing.JFrame
+import javax.swing.JPanel
 import javax.swing.SwingUtilities
 import javax.swing.UIManager
 import javax.swing.WindowConstants
+import javax.swing.border.EmptyBorder
 
-class ApplicationWindow : JFrame("HackWars") {
+class ApplicationWindow : JFrame() {
     private val activePanel = LoginPanel()
 
     init {
-        rootPane
         defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
         isResizable = false
+        if (System.getProperty("os.name").lowercase().contains("mac")) {
+            rootPane.putClientProperty("apple.awt.fullWindowContent", true)
+            rootPane.putClientProperty("apple.awt.transparentTitleBar", true)
+        }
+        rootPane.border = EmptyBorder(0, 0, 0, 0)
+        contentPane = JPanel(BorderLayout()).apply {
+            border = EmptyBorder(0, 0, 0, 0)
+            isOpaque = true
+            add(activePanel, BorderLayout.CENTER)
+        }
         addWindowListener(object : WindowAdapter() {
             override fun windowClosing(e: WindowEvent?) {
                 TODO("Handle closing down any running panels")
             }
         })
+        pack()
         setLocationRelativeTo(null)
         isVisible = true
-        contentPane = activePanel
-        pack()
     }
 }
 
