3eab7317ec55ce83d69dac7d248034411ef0f9ee
Sean Templeton <sean@templeton.io>
2026-03-19 07:35:28 -0500

    Remove incorrect casing

--- src/GameServer/java/Server/remote/RemoteCallHandler.kt
+++
@@ -1,7 +0,0 @@
-package server.remote
-
-import assignments.RemoteFunctionCall
-
-fun interface RemoteCallHandler {
-    fun handle(rfc: RemoteFunctionCall, context: RemoteCallContext)
-}
--- src/GameServer/java/Server/remote/RemoteFunctionCallDispatch.kt
+++
@@ -1,8 +0,0 @@
-package server.remote
-
-import assignments.RemoteFunctionCall
-import server.remote.generated.GeneratedRemoteCallRegistry
-
-fun RemoteFunctionCall.invokeOnServer(context: RemoteCallContext): Boolean {
-    return GeneratedRemoteCallRegistry.dispatch(this, context)
-}
